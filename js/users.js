a4fad39d18354c4e87b31cc783cf47d09ee9d14c
agustrova <agustinatrova@gmail.com>
2023-10-27 12:46:10 -0300

    editar casi terminado

--- js/users.js
+++ js/users.js
@@ -7,7 +7,7 @@ const usersArray = [
     active: true,
     password: 'password123',
     bornDate: new Date('1993-01-01').getTime(),
-    location: 'New York, NY',
+    location: 'Buenos Aires',
     image: 'https://oyster.ignimgs.com/mediawiki/apis.ign.com/mario-kart-for-wii-u/7/71/Mk8iconyoshi.png?width=1280'
     },
     {
@@ -18,7 +18,7 @@ const usersArray = [
       active: false,
       password: 'password456',
       bornDate: new Date('1998-05-05').getTime(),
-      location: 'Los Angeles, CA',
+      location: 'Chubut',
       image: 'https://oyster.ignimgs.com/mediawiki/apis.ign.com/mario-kart-for-wii-u/f/f5/Mk8icondaisy.png?width=1280'
     },
     {
@@ -112,27 +112,130 @@ const usersArray = [
 ];
 // Obtener el body de la tabla
 const tableBody = document.getElementById('table-body')
-
 const searchInput = document.querySelector('#search') 
+const userForm = document.querySelector("form#user-form")
+const submitBtn = userForm.querySelector('button[type=submit].btn-form')
 
+
+//Escuchamos el evento submit en el formulario
+userForm.addEventListener("submit", (evt) => {
+
+evt.preventDefault()
+
+const el = evt.target.elements
+
+//obtener los datos del form
+//console.dir(evt.target.elements.fullname.value)
+
+//deberia cortar la ejecucion de la funcion callback del evento submit cuando password y password2 sean distintas
+
+if(el.password.value !== el.password2.value) {
+  alert(`Las contraseñas no coinciden`)
+  return
+}
+
+/*
+//si el email ya existe
+const emailExist = usersArray.find((user) => {
+if (user.email === el.email.value)
+return true
+})
+
+if(emailExist) {
+  alert(`El correo ya se encuentra registrado`)
+  return
+}
+*/
+
+/*
+let id 
+if(el.id.value) {
+  id = el.id.value 
+} else {
+  id = crypto.randomUUID()
+}
+*/
+
+// # OPERADOR TERNARIO 
+// permite resolver if y else en una sola linea (para resolver lo de arriba en una sola linea)
+
+//const id = condicion ? condicionTrue : condicionFalse
+
+//           condicion        true          false
+const id = el.id.value ? el.id.value : crypto.randomUUID()  
+
+
+
+const user = {
+  fullname: el.fullname.value,
+  age: el.age.valueAsNumber, //obtengo el valor numerico
+  email: el.email.value,
+  password: el.password.value,
+  password2: el.password2.value,
+  active: el.active.checked,
+  bornDate: new Date (el.bornDate.value).getTime(),
+  location: el.location.value,
+  id: id,
+  image: el.image.value,
+}
+
+//tenemos 2 posibles acciones a realizar
+// Opcion 2 = edito un usuario y reemplazar un usuario
+// opcion  1 = agregue un usuario nuevo
+
+//pregunto si tengo id para saber si estoy editando o no
+if (el.id.value) {
+  //editando
+  const indice = usersArray.findIndex(usuario => {
+    if(usuario.id === el.id.value) {
+      return true
+    }
+  })
+  // reemplazo al usuario con los datos nuevos del form
+  usersArray[indice] = user
+  Swal.fire({
+    title: 'Usuario editado',
+    text: 'Los datos del usuario fueron actualizados correctamente',
+    icon:'success',
+    timer: 1000
+  })
+
+} else {
+  //agregando un usuario nuevo
+  usersArray.push(user)
+  Swal.fire('Usuario agregado', 'El usuario se creó correctamente', 'success')
+}
+
+pintarUsuarios(usersArray)
+resetearFormulario()
+} )
+
+function resetearFormulario() {
+  userForm.reset()
+  userForm.elements.password.disabled = false
+  userForm.elements.password2.disabled = false 
+  submitBtn.classList.remove ('btn-edit')
+  submitBtn.innerText = 'Agregar usuario'
+}
+
+// Filtro de usuarios
 //escuchar cuando el usuario selecciona una tecla en el input search
-//obtener el valor del input
-//buscar en todos los usuarios aquellos donde su nombre tengan este texto
-//pintar solo los usuarios que hayan coincidido
 searchInput.addEventListener ('keyup', (eventito) => {
-
-//buscar en todos los usuarios aquellos donde su nombre tengan ese texto
-
+//obtener el valor del input
 const inputValue = eventito.target.value.toLowerCase()
-
+//buscar en todos los usuarios aquellos donde su nombre tengan este texto
 const usuariosFiltrados = 
 usersArray.filter((usuario) => {
 
 const nombre = usuario.fullname.toLowerCase()
 
 return nombre.includes(inputValue) 
-//no hace falta el if porque sino, es falso
 })
+//pintar solo los usuarios que hayan coincidido
+//buscar en todos los usuarios aquellos donde su nombre tengan ese texto
+
+//no hace falta el if porque sino, es falso
+
 
 //Pintar solo los usuarios que hayan coincidido
 pintarUsuarios(usuariosFiltrados)
@@ -161,6 +264,13 @@ function pintarUsuarios(arrayPintar) {
         <button class="action-btn btn-danger" title="Borrar usuario" onclick="borrarUsuario(${index})">
         <i class="fa-solid fa-trash-can"></i>
         </button>
+        <button class="action-btn"
+                title="Editar usuario"
+                onclick="editarUsuario('${user.id}')">
+                <i class="fa-solid fa-pen-to-square"></i>
+        </button>
+
+        </td>
     </tr>`
     //ahi en formatdate user.borndate lo que hice fue definir la funcion formatdate y despues le pongo el nombre fecha porque es lo mismo, le puedo poner el nombre que quiera
 })
@@ -182,32 +292,87 @@ function borrarUsuario (indice) {
 
 
 
-function formatDate (fecha) {
-  const collator = new Intl.DateTimeFormat(`es-AR`, {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric'
+const objeto = {
+  nombre: "pepito",
+  apellid: "perez",
+  saludar () {
+    console.log("Hola mi nombre es pepito")
+  }
+}
+
+objeto.saludar
+
+
+// forEach, map, filter, findIndex, find, flat, flatMap, every, some. Todos estos metodos trabajan agarrando el primer elemento y le pones el nombre que quieras
+
+function editarUsuario(idBuscar) {
+  //buscar un usuario con id y obtenerlo
+  const userEdit = usersArray.find((usuario) => {
+
+    //deberia devolver un true, segun la condicion id que me enviaron === al del usuario que estoy iterando
+    if(usuario.id === idBuscar) {
+      return true
+    }
+
   })
 
-  const fechaFormateada = collator.format(fecha)
-// devolver una fecha en el format document
+  //indicar que el usuario no fue encontrado
+  //if(userEdit === undefined) 
+  // si no es true, o sea si no existe va con ! is not
+  if(!userEdit) {
+    //console.warn(`El usuario a editar no existe`)
+    Swal.fire('Error', 'No se pudo editar el usuario', 'error')
+    return
+  }
 
-console.log(fechaFormateada)
+  console.log(userEdit)
 
-return fechaFormateada
+  //rellenar el formulario con los datos del usuario a editar
+
+  const el = userForm.elements
+
+  el.id.value = userEdit.id
+  el.age.value = userEdit.age
+el.fullname.value = userEdit.fullname
+el.email.value = userEdit.email
+el.image.value = userEdit.image
+el.location.value = userEdit.location
+el.active.checked = userEdit.active
+
+// deshaiblitar los input de la contraseña
+el.password.value = userEdit.password
+el.password.disabled = true
+el.password2.value = userEdit.password
+
+
+el.password2.disabled = true
+el.bornDate.value = formatInputDate(userEdit.bornDate)
+
+console.log(formatInputDate(userEdit.bornDate))
+  //cambiar el nombre del boton a editar usuario
+
+submitBtn.classList.add('btn-edit')
+submitBtn.innerText = 'Editar usuario'
+
+
+  
+
+
+
+
+
+
+
+
+
+
 
 }
 
 
 
 
-
-
-
-
-
-
-{/* 
+/* 
 <tr class="table-body">
     <td class="user-image">
         <img src="https://m.media-amazon.com/images/I/81wNRtDaTXL.png" alt="Imagen de prueba">
@@ -217,4 +382,4 @@ return fechaFormateada
     <td class="user-location">Super Mario World</td>
     <td class="user-age">24</td>
     <td class="user-date">24/05/98</td>
-</tr> */}
+</tr> */
